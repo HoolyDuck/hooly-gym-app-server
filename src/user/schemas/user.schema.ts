9ffde8e764acc392f0c97c21447422e6a9826344
HoolyDuck <nazar260804@gmail.com>
2024-08-24 02:21:35 +0300

    * user things

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,42 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ type: { provider: String, id: String } })
+  oauthAccounts: {
+    provider: string;
+    id: string;
+  }[];
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+const UserSchema = SchemaFactory.createForClass(User);
+
+// hash password before saving
+UserSchema.pre<UserDocument>('save', async function (next) {
+  if (this.password) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
+});
+
+export { UserSchema };
---
+++ src/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/user/schemas/user.schema';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = await this.userModel.create(createUserDto);
+    return createdUser;
+  }
+
+  async findOne(id: string): Promise<User> {
+    return this.userModel.findById(id).exec();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  async findByGoogleId(id: string): Promise<User> {
+    return this.userModel.findOne({
+      oauthAccounts: { $elemMatch: { provider: 'google', id } },
+    });
+  }
+}

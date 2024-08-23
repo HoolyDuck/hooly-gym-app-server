3f713ffe78759f4d333a9cd9f01f1330181d0c66
HoolyDuck <nazar260804@gmail.com>
2024-08-24 02:21:52 +0300

    + config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import dbConfig from './common/config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: dbConfig,
+      
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
